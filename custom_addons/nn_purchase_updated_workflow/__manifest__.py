57fd5552a156d8986ea177ef95676d0fa36c3197
IbrahimOuchari <ibrahim.o@neonara.digital>
2025-06-09 08:30:09 +0100

    Purchase : - fixed the manifest purchase error purchase order report

--- custom_addons/nn_purchase_updated_workflow/__manifest__.py
+++ custom_addons/nn_purchase_updated_workflow/__manifest__.py
@@ -10,7 +10,6 @@ Ce module met à jour les vues personnalisées et le workflow du module d'Achat.
     'website': 'https://neonara.digital',
     'depends': ['purchase','base'],
     'data': [
-        'reports/purchase_order_report.xml',
 
         # 'data/email_template_edi_purchase.xml',
         'views/purchase_order_views.xml',
